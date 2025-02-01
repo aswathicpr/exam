9ae7a4fdf79cadd3e77accf113aa1d1cfef0452a
ikmlap337 <aswathicpr@gmail.com>
2025-02-01 14:26:37 +0530

    new project

---
+++ ptx-exam/src/main/java/com/ikm/ptx_exam/PtxExamApplication.java
@@ -0,0 +1,13 @@
+package com.ikm.ptx_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtxExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtxExamApplication.class, args);
+	}
+
+}
---
+++ ptx-exam/src/test/java/com/ikm/ptx_exam/PtxExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikm.ptx_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtxExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
