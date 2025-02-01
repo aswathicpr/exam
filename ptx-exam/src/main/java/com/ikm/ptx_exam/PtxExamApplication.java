46546f4c1ea5f4792c85cc997fca6e59a9aee9d4
aswathicpr <166105878+aswathicpr@users.noreply.github.com>
2025-02-01 14:31:56 +0530

    Update PtxExamApplication.java

--- ptx-exam/src/main/java/com/ikm/ptx_exam/PtxExamApplication.java
+++ ptx-exam/src/main/java/com/ikm/ptx_exam/PtxExamApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PtxExamApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(PtxExamApplication.class, args);
+		System.out.println("Welcome");
 	}
 
 }
